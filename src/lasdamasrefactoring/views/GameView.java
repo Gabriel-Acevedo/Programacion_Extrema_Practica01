6cac4f35b0ccdf9f745189213e022a283c5bb259
Gabriel Gordo <gabriel.gordo@acevedoapps.com>
2019-11-09 15:09:25 +0100

    GameView - Remove static variable.

--- src/lasdamasrefactoring/views/GameView.java
+++ src/lasdamasrefactoring/views/GameView.java
@@ -8,24 +8,22 @@ import lasdamasrefactoring.models.Coordinate;
 
 public class GameView extends WithConsoleView {
 
-    private static final String[] COLORS = new String[]{"b", "n", " "};
-
 	public void write(Controller controller) {
-        final int DIMENSION = controller.getDimension();
-        this.writeNumbersLine(DIMENSION);
-        for(int i=0; i<DIMENSION; i++){
-            this.console.write((i+1)+"");
-            for(int j=0; j<DIMENSION; j++){
-                Color color = controller.getColor(new Coordinate(i,j));
-                if (color == null){
-                    this.console.write(GameView.COLORS[2]);
-                } else {
-                    this.console.write(GameView.COLORS[color.ordinal()]);
+            final int DIMENSION = controller.getDimension();
+            this.writeNumbersLine(DIMENSION);
+            for(int i=0; i<DIMENSION; i++){
+                this.console.write((i+1)+"");
+                for(int j=0; j<DIMENSION; j++){
+                    Color color = controller.getColor(new Coordinate(i,j));
+                    if (color == null){
+                        this.console.write(MessageView.COLORSChar[2]);
+                    } else {
+                        this.console.write(MessageView.COLORSChar[color.ordinal()]);
+                    }
                 }
+                this.console.writeln((i+1)+"");
             }
-            this.console.writeln((i+1)+"");
-        }
-        this.writeNumbersLine(DIMENSION);
+            this.writeNumbersLine(DIMENSION);
 	}
 
     private void writeNumbersLine(final int DIMENSION) {
