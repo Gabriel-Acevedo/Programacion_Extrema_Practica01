82d657f20410c76ad83914f5bb0f23c1635e539e
Gabriel Gordo <gabriel.gordo@acevedoapps.com>
2019-11-09 15:49:56 +0100

    Create New Class Checkers with blank methods.

---
+++ src/lasdamasrefactoring/controllers/Checkers.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lasdamasrefactoring.controllers;
+
+/**
+ *
+ * @author Gabri
+ */
+public class Checkers {
+    
+    public Error GeneralMovementCheck(){
+        return null;
+        
+    }
+    
+    public Error PieceMovementCheck(){
+        return null;  
+    } 
+    
+}
