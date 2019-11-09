244cc102a6af5b57f6486d642db4c8d7db5f2198
Gabriel Gordo <gabriel.gordo@acevedoapps.com>
2019-11-09 17:40:02 +0100

    Renamed PieceTest Tests methods.

--- test/lasdamasrefactoring/models/PieceTest.java
+++ test/lasdamasrefactoring/models/PieceTest.java
@@ -8,13 +8,13 @@ import org.junit.Test;
 public class PieceTest {
 
     @Test
-    public void testGivenPieceWhenIsAdvancedThenTrue(){
+    public void PieceIsAdvancedThenTrueTest(){
         assertTrue(new Piece(Color.WHITE).isAdvanced(new Coordinate(5,0), new Coordinate(4,1)));
         assertTrue(new Piece(Color.BLACK).isAdvanced(new Coordinate(2,1), new Coordinate(3,2)));
     }
 
     @Test
-    public void testGivenPieceWhenNotIsAdvancedThenFalse(){
+    public void PieceIsNotAdvancedThenFalseTest(){
         assertFalse(new Piece(Color.WHITE).isAdvanced(new Coordinate(5,0), new Coordinate(6,1)));
         assertFalse(new Piece(Color.WHITE).isAdvanced(new Coordinate(5,0), new Coordinate(5,2)));
         assertFalse(new Piece(Color.BLACK).isAdvanced(new Coordinate(2,1), new Coordinate(2,3)));
